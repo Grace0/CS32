74a0b4d9930e186bc1f2e62b4d09013d745961e3
Grace Kwak <gracekwak25@gmail.com>
2021-01-09 12:00:46 -0800

    separated flatulan.h

---
+++ CS32/Flatulan.cpp
@@ -0,0 +1,8 @@
+//
+//  Flatulan.cpp
+//  CS32
+//
+//  Created by Grace Kwak on 1/7/21.
+//
+
+#include "Flatulan.h"
---
+++ CS32/Flatulan.h
@@ -0,0 +1,33 @@
+//
+//  Flatulan.hpp
+//  CS32
+//
+//  Created by Grace Kwak on 1/7/21.
+//
+
+#ifndef Flatulan_h
+#define Flatulan_h
+
+class City;
+
+class Flatulan
+{
+  public:
+        // Constructor
+    Flatulan(City* cp, int r, int c);
+
+        // Accessors
+    int  row() const;
+    int  col() const;
+
+        // Mutators
+    void move();
+    bool possiblyGetConverted();
+
+  private:
+    City* m_city;
+    int   m_row;
+    int   m_col;
+};
+
+#endif /* Flatulan_hpp */

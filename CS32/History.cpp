e2c0f3ec52288bd9aded2cc5f5a80a3441c6b4b5
Grace Kwak <gracekwak25@gmail.com>
2021-01-10 10:50:21 -0800

    started doing the history feature implementation

--- CS32/History.cpp
+++ CS32/History.cpp
@@ -6,3 +6,17 @@
 //
 
 #include "History.h"
+#include "utilities.cpp"
+
+History::History(int nRows, int nCols)
+{
+    
+}
+
+bool History::record(int r, int c) {
+    return false;
+}
+
+void History::display() const {
+    clearScreen();
+}
--- CS32/History.h
+++ CS32/History.h
@@ -8,6 +8,15 @@
 #ifndef History_h
 #define History_h
 
-#include <stdio.h>
+class History
+{
+  public:
+    History(int nRows, int nCols);
+    bool record(int r, int c);
+    void display() const;
+
+private:
+   
+};
 
 #endif /* History_hpp */

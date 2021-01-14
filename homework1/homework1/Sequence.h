3bcc1756f666e84cc12fedf1bd311118c6d76c58
Grace Kwak <gracekwak25@gmail.com>
2021-01-13 21:40:21 -0800

    completed #2

--- homework1/homework1/Sequence.h
+++ homework1/homework1/Sequence.h
@@ -5,13 +5,15 @@
 //  Created by Grace Kwak on 1/13/21.
 //
 
+using ItemType = std::string;
+
 class Sequence
 {
   public:
     Sequence();    // Create an empty sequence (i.e., one with no items)
     bool empty() const;  // Return true if the sequence is empty, otherwise false.
     int size() const;    // Return the number of items in the sequence.
-    int insert(int pos, const std::string& value);
+    int insert(int pos, const ItemType& value);
       // Insert value into the sequence so that it becomes the item at
       // position pos.  The original item at position pos and those that
       // follow it end up at positions one higher than they were at before.
@@ -21,7 +23,7 @@ class Sequence
       // leave the sequence unchanged and return -1.  Notice that
       // if pos is equal to size(), the value is inserted at the end.
 
-    int insert(const std::string& value);
+    int insert(const ItemType& value);
       // Let p be the smallest integer such that value <= the item at
       // position p in the sequence; if no such item exists (i.e.,
       // value > all items in the sequence), let p be size().  Insert
@@ -37,21 +39,21 @@ class Sequence
       // positions one lower than they were at before), and return true.
       // Otherwise, leave the sequence unchanged and return false.
      
-    int remove(const std::string& value);
+    int remove(const ItemType& value);
       // Erase all items from the sequence that == value.  Return the
       // number of items removed (which will be 0 if no item == value).
 
-    bool get(int pos, std::string& value);
+    bool get(int pos, ItemType& value);
       // If 0 <= pos < size(), copy into value the item at position pos
       // of the sequence and return true.  Otherwise, leave value unchanged
       // and return false.
 
-    bool set(int pos, const std::string& value);
+    bool set(int pos, const ItemType& value);
       // If 0 <= pos < size(), replace the item at position pos in the
       // sequence with value and return true.  Otherwise, leave the sequence
       // unchanged and return false.
 
-    int find(const std::string& value) const;
+    int find(const ItemType& value) const;
       // Let p be the smallest integer such that value == the item at
       // position p in the sequence; if no such item exists, let p be -1.
       // Return p.

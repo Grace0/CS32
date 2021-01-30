6f710449bb5ce5c6cb2c8f8b6683f8f4cd430817
Grace Kwak <gracekwak25@gmail.com>
2021-01-30 12:09:32 -0800

    getting there

--- homework2/homework2/mazestack.cpp
+++ homework2/homework2/mazestack.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <string>
 #include <stack>
+#include <cassert>
 
 using namespace std;
 
@@ -22,18 +23,67 @@ class Coord
     int m_col;
 };
 
-bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int ec);
+bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int ec) {
+    
+    stack<Coord> coordStack;     // declare a stack of Coords
+
+    //Push the starting coordinate (sr,sc) onto the coordinate stack and
+    //    update maze[sr][sc] to indicate that the algorithm has encountered
+    //    it (i.e., set maze[sr][sc] to have a value other than '.').
+    Coord start(sr,sc);
+    coordStack.push(start);
+    maze[sr][sc] = '#';
+    
+    int rr, cc;
+    
+    while (!coordStack.empty()) { //While the stack is not empty,
+        
+        //{   Pop the top coordinate off the stack. This gives you the current
+        //        (r,c) location that your algorithm is exploring.
+        Coord next = coordStack.top();
+        coordStack.pop();
+        
+        //    If the current (r,c) coordinate is equal to the ending coordinate,
+        //        then we've solved the maze so return true!
+        if (next.r() == er && next.c() == ec) {
+            return true;
+        }
+        
+        rr = next.r();
+        cc = next.c();
+        
+        if (maze[rr-1][cc] == '.') {
+            Coord north(rr-1, cc);
+            coordStack.push(north);
+            maze[rr-1][cc] = '#';
+        } if (maze[rr][cc+1] == '.') {
+            Coord east(rr, cc+1);
+            coordStack.push(east);
+            maze[rr][cc+1] = '#';
+        } if (maze[rr+1][cc] == '.') {
+            Coord south(rr+1, cc);
+            coordStack.push(south);
+            maze[rr+1][cc] = '#';
+        } if (maze[rr][cc-1] == '.') {
+            Coord west(rr, cc-1);
+            coordStack.push(west);
+            maze[rr][cc-1] = '#';
+        }
+        
+    }
+    
+    //There was no solution, so return false
+    return false;
+  
+  
+}
           // Return true if there is a path from (sr,sc) to (er,ec)
           // through the maze; return false otherwise
 
-//Push the starting coordinate (sr,sc) onto the coordinate stack and
-//    update maze[sr][sc] to indicate that the algorithm has encountered
-//    it (i.e., set maze[sr][sc] to have a value other than '.').
-//While the stack is not empty,
-//{   Pop the top coordinate off the stack. This gives you the current
-//        (r,c) location that your algorithm is exploring.
-//    If the current (r,c) coordinate is equal to the ending coordinate,
-//        then we've solved the maze so return true!
+
+
+
+
 //    Check each place you can move from the current cell as follows:
 //        If you can move NORTH and haven't encountered that cell yet,
 //            then push the coordinate (r-1,c) onto the stack and update
@@ -48,40 +98,39 @@ bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er, int
 //            then push the coordinate (r,c-1) onto the stack and update
 //            maze[r][c-1] to indicate the algorithm has encountered it.
 //}
-//There was no solution, so return false
+
 
 
 int main() {
     
-    stack<Coord> coordStack;     // declare a stack of Coords
-
-    Coord a(5,6);
-    coordStack.push(a);          // push the coordinate (5,6)
-    coordStack.push(Coord(3,4)); // push the coordinate (3,4)
-   
-    Coord b = coordStack.top();  // look at top item in the stack
-    coordStack.pop();            // remove the top item from stack
-    if (coordStack.empty())      // Is the stack empty?
-        cout << "empty!" << endl;
-    cout << coordStack.size() << endl;  // number of elements
-    
-//    string maze[10] = {
-//        "XXXXXXXXXX",
-//        "X........X",
-//        "XX.X.XXXXX",
-//        "X..X.X...X",
-//        "X..X...X.X",
-//        "XXXX.XXX.X",
-//        "X.X....XXX",
-//        "X..XX.XX.X",
-//        "X...X....X",
-//        "XXXXXXXXXX"
+//    string maze1[4] = {
+//        "XXX",
+//        "X.X",
+//        "X.X",
+//        "XXX"
 //    };
 //
-//    if (pathExists(maze, 10,10, 6,4, 1,1))
-//        cout << "Solvable!" << endl;
-//    else
-//        cout << "Out of luck!" << endl;
+//    cout << pathExists(maze1, 4,3, 2,1, 1,1) << endl;
+    
+    string maze[10] = {
+        "XXXXXXXXXX",
+        "X........X",
+        "XX.X.XXXXX",
+        "X..X.X...X",
+        "X..X...X.X",
+        "XXXX.XXX.X",
+        "X.X....XXX",
+        "X..XX.XX.X",
+        "X...X....X",
+        "XXXXXXXXXX"
+    };
 
+    assert(pathExists(maze, 10,10, 6,4, 1,1) == 1);
+    assert(pathExists(maze, 10,10, 6,4, 7,8) == 0);
+    assert(pathExists(maze, 10,10, 6,4, 7,1) == 0);
+    assert(pathExists(maze, 10,10, 6,4, 5,8) == 1);
+    assert(pathExists(maze, 10,10, 6,4, 1,5) == 1);
+
+    cerr << "Passed all tests" << endl;
     return 0;
 }

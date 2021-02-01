d73d83b513141c1ef414146fac8bd26034ba08ba
Grace Kwak <gracekwak25@gmail.com>
2021-01-31 21:04:32 -0800

    starting #5

---
+++ homework2/homework2/eval.cpp
@@ -0,0 +1,95 @@
+//
+//  eval.cpp
+//  homework2
+//
+//  Created by Grace Kwak on 1/30/21.
+//
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <cassert>
+
+using namespace std;
+
+//#include lines you need
+//
+//        declarations of any additional functions you might have written
+//                to help you evaluate an expression
+//
+//        int evaluate(string infix, const bool values[], string& postfix, bool& result)
+//        {
+//            your expression evaluation code
+//        }
+//
+//        implementations of any additional functions you might have written
+//                to help you evaluate an expression
+//
+//        a main routine to test your function
+
+int evaluate(string infix, const bool values[], string& postfix, bool& result) {
+    stack<char> evalStack;
+    
+    char currChar;
+    
+    for (int i = 0; i < postfix.length(); i++) {
+        currChar = postfix[i];
+        if (currChar ) {
+            
+        }
+    }
+    return 1;
+}
+          // Evaluates a boolean expression
+          //   If infix is a syntactically valid infix boolean expression,
+          //   then set postfix to the postfix form of that expression, set
+          //   result to the value of the expression (where in that expression,
+          //   each digit k represents element k of the values array), and
+          //   return zero.  If infix is not a syntactically valid expression,
+          //   return 1.  (In that case, postfix may or may not be changed, but
+          //   result must be unchanged.)
+
+int main() {
+//    0     1     2     3     4     5     6     7     8     9
+//  true  true  true  false false false true  false true  false
+//
+//  2                          evaluates to true
+//  (3)                        evaluates to false
+//  2&(3)                      evaluates to false
+//  0 & !9                     evaluates to true
+//  !(7|8)                     evaluates to false
+//  !7|8                       evaluates to true
+//  6|4&5                      evaluates to true
+//  1&!(9|1&1|9) | !!!(9&1&9)  evaluates to true
+    
+//    bool ba[10] = {
+//                  //  0      1      2      3      4      5      6      7      8      9
+//                    true,  true,  true,  false, false, false, true,  false, true,  false
+//                };
+//                string pf;
+//                bool answer;
+//                assert(evaluate("2| 3", ba, pf, answer) == 0  &&  pf == "23|" &&  answer);
+//                assert(evaluate("8|", ba, pf, answer) == 1);
+//                assert(evaluate(" &6", ba, pf, answer) == 1);
+//                assert(evaluate("4 5", ba, pf, answer) == 1);
+//                assert(evaluate("01", ba, pf, answer) == 1);
+//                assert(evaluate("()", ba, pf, answer) == 1);
+//                assert(evaluate("()4", ba, pf, answer) == 1);
+//                assert(evaluate("2(9|8)", ba, pf, answer) == 1);
+//                assert(evaluate("2(&8)", ba, pf, answer) == 1);
+//                assert(evaluate("(6&(7|7)", ba, pf, answer) == 1);
+//                assert(evaluate("x+5", ba, pf, answer) == 1);
+//                assert(evaluate("", ba, pf, answer) == 1);
+//                assert(evaluate("2|3|4", ba, pf, answer) == 0
+//                                       &&  pf == "23|4|"  &&  answer);
+//                assert(evaluate("2|(3|4)", ba, pf, answer) == 0
+//                                       &&  pf == "234||"  &&  answer);
+//                assert(evaluate("4  |  !3 & (0&3) ", ba, pf, answer) == 0
+//                                       &&  pf == "43!03&&|"  &&  !answer);
+//                assert(evaluate(" 9  ", ba, pf, answer) == 0  &&  pf == "9"  &&  !answer);
+//                ba[2] = false;
+//                ba[9] = true;
+//                assert(evaluate("((9))", ba, pf, answer) == 0  &&  pf == "9"  &&  answer);
+//                assert(evaluate("2| 3", ba, pf, answer) == 0  &&  pf == "23|" &&  !answer);
+//                cout << "Passed all tests" << endl;
+}

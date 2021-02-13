30b1095793b51d721e8157a568e0fcd04add1c2d
Grace Kwak <gracekwak25@gmail.com>
2021-02-12 16:59:21 -0800

    finished #3

--- homework4/homework4/test.cpp
+++ homework4/homework4/test.cpp
@@ -1,22 +1,22 @@
-#include "Sequence.h"  // class template from problem 1
-
-    class Coord
-    {
-      public:
-        Coord(int rr, int cc) : m_row(rr), m_col(cc) {}
-        Coord() : m_row(0), m_col(0) {}
-        double r() const { return m_row; }
-        double c() const { return m_col; }
-      private:
-        double m_row;
-        double m_col;
-    };
-
-    int main()
-    {
-        Sequence<int> si;
-        si.insert(50);               // OK
-        Sequence<Coord> sc;
-        sc.insert(0, Coord(50,20));  // OK
-        sc.insert(Coord(40,10));     // error!
-    }
+//#include "Sequence.h"  // class template from problem 1
+//
+//    class Coord
+//    {
+//      public:
+//        Coord(int rr, int cc) : m_row(rr), m_col(cc) {}
+//        Coord() : m_row(0), m_col(0) {}
+//        double r() const { return m_row; }
+//        double c() const { return m_col; }
+//      private:
+//        double m_row;
+//        double m_col;
+//    };
+//
+//    int main()
+//    {
+//        Sequence<int> si;
+//        si.insert(50);               // OK
+//        Sequence<Coord> sc;
+//        sc.insert(0, Coord(50,20));  // OK
+//        sc.insert(Coord(40,10));     // error!
+//    }

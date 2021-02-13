c77b0d8c40fff20fea2ed1695c330d64df12f5ec
Grace Kwak <gracekwak25@gmail.com>
2021-02-12 18:27:04 -0800

    init proj3

---
+++ project3/project3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project3
+//
+//  Created by Grace Kwak on 2/12/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

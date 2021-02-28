2355c3177e57efe685f472fa23848f9786279cce
Grace Kwak <gracekwak25@gmail.com>
2021-02-28 13:38:17 -0800

    finished #4

--- homework4/homework4/list.cpp
+++ homework4/homework4/list.cpp
@@ -1,67 +1,78 @@
-#include <iostream>
-#include <string>
-#include <vector>
-
-using namespace std;
-
-class File
-{
-  public:
-    File(string nm) : m_name(nm) {}
-    virtual ~File() {}
-    string name() const { return m_name; }
-    virtual bool add(File* f) = 0;
-    virtual const vector<File*>* files() const = 0;
-  private:
-    string m_name;
-};
-
-class PlainFile : public File
-{
-  public:
-    PlainFile(string nm) : File(nm) {}
-    virtual bool add(File* f) { return false; }
-    virtual const vector<File*>* files() const { return nullptr; }
-};
-
-class Directory : public File
-{
-  public:
-    Directory(string nm) : File(nm) {}
-    virtual ~Directory();
-    virtual bool add(File* f) { m_files.push_back(f); return true; }
-    virtual const vector<File*>* files() const { return &m_files; }
-  private:
-    vector<File*> m_files;
-};
-
-Directory::~Directory()
-{
-    for (int k = 0; k < m_files.size(); k++)
-        delete m_files[k];
-}
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//class File
+//{
+//  public:
+//    File(string nm) : m_name(nm) {}
+//    virtual ~File() {}
+//    string name() const { return m_name; }
+//    virtual bool add(File* f) = 0;
+//    virtual const vector<File*>* files() const = 0;
+//  private:
+//    string m_name;
+//};
+//
+//class PlainFile : public File
+//{
+//  public:
+//    PlainFile(string nm) : File(nm) {}
+//    virtual bool add(File* f) { return false; }
+//    virtual const vector<File*>* files() const { return nullptr; }
+//};
+//
+//class Directory : public File
+//{
+//  public:
+//    Directory(string nm) : File(nm) {}
+//    virtual ~Directory();
+//    virtual bool add(File* f) { m_files.push_back(f); return true; }
+//    virtual const vector<File*>* files() const { return &m_files; }
+//  private:
+//    vector<File*> m_files;
+//};
+//
+//Directory::~Directory()
+//{
+//    for (int k = 0; k < m_files.size(); k++)
+//        delete m_files[k];
+//}
 
 void listAll(const File* f, string path)  // two-parameter overload
 {
     //Base case: we've hit a PlanFile
-    if (f->files() == nullptr) cout << path + "/" + f->name() << endl; //base case
+    if (f->files() == nullptr) {
+        cout << path + "/" + f->name() << endl; //base case
+        return;
+    }
     
-//    for (vector<File*>::const_iterator it = f->files()->begin(); it != f->files()->end(); it++) { //magic function that can do everything except the root file
-//        listAll(*it, "/" + f->name());
-//    }
+    //Other base case: we've hit an empty Directory
+    if (f->files()->size() == 0)  {
+        cout << path + "/" + f->name() << endl; //base case
+        return;
+    }
+    
+    cout << path + "/" + f->name() << endl; //base case
+    
+    //We've hit a Directory, so we want to iterate through the directory's Files
+    for (vector<File*>::const_iterator it = f->files()->begin(); it != f->files()->end(); it++) { //magic function that can do everything except the root file
+        listAll(*it, path + "/" + f->name());
+    }
     
 }
-
-void listAll(const File* f)  // one-parameter overload
-{
-    if (f != nullptr)
-        listAll(f, "");
-}
-
-int main()
-{
-    cout << "que";
-    PlainFile* d1 = new PlainFile("Fun");
+//
+//void listAll(const File* f)  // one-parameter overload
+//{
+//    if (f != nullptr)
+//        listAll(f, "");
+//}
+//
+//int main()
+//{
+//    Directory* d1 = new Directory("Fun");
 //    d1->add(new PlainFile("party.jpg"));
 //    d1->add(new PlainFile("beach.jpg"));
 //    d1->add(new PlainFile("skitrip.jpg"));
@@ -72,6 +83,6 @@ int main()
 //    d3->add(new PlainFile("me.jpg"));
 //    d3->add(new Directory("Miscellaneous"));
 //    d3->add(d2);
-    listAll(d1);
-    delete d1;
-}
+//    listAll(d3);
+//    delete d3;
+//}
